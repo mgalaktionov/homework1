c9862f68e0c121406b7bff296b86dc1562896803
mikhail <mgalaktionov4@gmail.com>
2020-10-22 11:00:20 +0300

    помянял реализацию HttpClient

--- main-module/src/main/java/ru/digitalhabbits/homework1/service/WikipediaClient.java
+++ main-module/src/main/java/ru/digitalhabbits/homework1/service/WikipediaClient.java
@@ -10,38 +10,56 @@ import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
 
 import javax.annotation.Nonnull;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.logging.Logger;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
 
 public class WikipediaClient {
     private final int SUCCESS_HTTP_STATUS_CODE = 200;
     public static final String WIKIPEDIA_SEARCH_URL = "https://en.wikipedia.org/w/api.php";
-    private final Logger logger = Logger.getAnonymousLogger();
+    private final Logger logger = getLogger(WikipediaClient.class);
 
     @Nonnull
     public String search(@Nonnull String searchString) throws IOException {
         final URI uri = prepareSearchUrl(searchString);
         // TODO: NotImplemented
-        HttpClient client = HttpClients.createDefault();
-        System.out.println("Request url = "+prepareSearchUrl(searchString).toString());
+        //Creating get request to wikipedia api
         HttpGet getReq = prepareHttpGet(prepareSearchUrl(searchString));
-        HttpResponse response = null;
+        CloseableHttpClient client = HttpClients.createDefault();
+        logger.info("Request url = "+prepareSearchUrl(searchString).toString());
+        CloseableHttpResponse response;
         try {
             response = client.execute(getReq);
+            try{
+                // Cheking response status
+                if (response != null && getResponseCode(response) == SUCCESS_HTTP_STATUS_CODE) {
+                    return entityToString(response.getEntity());
+                } else {
+                    logger.error("Something went wrong with response, programm will exit");
+                    System.exit(1);
+                }
+            }finally {
+                response.close();
+            }
         } catch (IOException e) {
             logger.info("IO exception was caught, system exit");
             e.printStackTrace();
             System.exit(1);
-        }
-        if (response != null && response.getStatusLine().getStatusCode() == SUCCESS_HTTP_STATUS_CODE) {
-            return entityToString(response.getEntity());
+        }finally {
+            client.close();
         }
         return "";
+    }
+
+    private int getResponseCode(CloseableHttpResponse response){
+        return response.getStatusLine().getStatusCode();
     }
 
     private String entityToString(@Nonnull HttpEntity entity) throws IOException {
