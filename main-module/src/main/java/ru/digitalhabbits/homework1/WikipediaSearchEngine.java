c9afe64e1cfc07f49d7bd81bd461bea5c503d6b3
mikhail <mgalaktionov4@gmail.com>
2020-10-28 17:50:13 +0300

    распараллелил поиск в википедии и остальные операции

--- main-module/src/main/java/ru/digitalhabbits/homework1/WikipediaSearchEngine.java
+++ main-module/src/main/java/ru/digitalhabbits/homework1/WikipediaSearchEngine.java
@@ -9,6 +9,7 @@ import ru.digitalhabbits.homework1.service.WikipediaClient;
 
 import javax.annotation.Nonnull;
 import java.util.List;
+import java.util.concurrent.*;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -18,12 +19,17 @@ public class WikipediaSearchEngine {
     private static final String PLUGIN_DIR = "plugins";
 
     public void search(@Nonnull String searchString) {
+        var startTime = System.currentTimeMillis();
         logger.info("Searching '{}' on wikipedia", searchString);
 
         // 1. сделать запрос в wikipedia, получить результат в формате json.
         final WikipediaClient client = new WikipediaClient();
-        final String text = client.search(searchString);
-        logger.info("FORMATTED TEXT \n {}", text);
+        var pool = Executors.newFixedThreadPool(3);
+        Callable<String> callable = new WikiCallable(searchString,client);
+        Future<String> futureText = pool.submit(callable);
+        //final String text = client.search(searchString);
+
+        //logger.info("FORMATTED TEXT \n {}", text);
 
         // 2. очистить папку с результатами
         final FileEngine fileEngine = new FileEngine();
@@ -36,15 +42,46 @@ public class WikipediaSearchEngine {
 
         // 4. выполнить действия над результатом и записать в файл
         final PluginEngine pluginEngine = new PluginEngine();
-        for (Class<? extends PluginInterface> plugin: plugins) {
-            if (plugin.getSimpleName().contains(PLUGIN_POSTFIX)) {
-                final String result = pluginEngine.applyPlugin(plugin, text);
+        String text = "";
+        try {
+            text = futureText.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+            pool.shutdown();
+        } finally {
+            pool.shutdown();
+        }
+        if(!text.isEmpty()) {
+            for (Class<? extends PluginInterface> plugin : plugins) {
+                if (plugin.getSimpleName().contains(PLUGIN_POSTFIX)) {
+                    final String result = pluginEngine.applyPlugin(plugin, text);
 
-                final String pluginName = plugin.getSimpleName();
-                logger.info("Apply '{}' plugin get result {}", pluginName, result);
+                    final String pluginName = plugin.getSimpleName();
+                    logger.info("Apply '{}' plugin get result {}", pluginName, result);
 
-                fileEngine.writeToFile(result, pluginName);
+                    fileEngine.writeToFile(result, pluginName);
+                }
             }
+        }
+        logger.info("DURATION OF PROCESSING '{}", System.currentTimeMillis() - startTime);
+    }
+
+    private static class WikiCallable implements Callable{
+
+        private final String searchString;
+        private final WikipediaClient client;
+
+        private WikiCallable(String searchString, WikipediaClient client) {
+            this.searchString = searchString;
+            this.client = client;
+        }
+
+        @Override
+        public String call() throws Exception {
+            logger.info("STARTED WIKISEARCH THREAD");
+            return client.search(searchString);
         }
     }
 }
